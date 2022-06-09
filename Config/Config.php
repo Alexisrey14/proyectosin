fd86aad24fc639df42478a5ecf5031bb8a3747af
Alexisrey14 <alexis.reyes@alum.udep.edu.pe>
2022-06-08 19:18:08 -0500

    Avance 1

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/proyectosin/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "proyectosin";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "PEN";
+const CLIENT_ID = "";
+?>
